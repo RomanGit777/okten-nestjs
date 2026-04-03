a171294afd9d8da67a97a5d13e33ade04d3277db
RomanGit777 <rsavenko91@gmail.com>
2026-04-03 21:04:44 +0200

    master

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TablesModule } from './tables/tables.module';
-import { TypeormModule } from './typeorm.module';
 
 @Module({
-  imports: [TablesModule, TypeormModule],
+  imports: [],
   controllers: [],
   providers: [],
 })
